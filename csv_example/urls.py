80b5f9e6064ad9f13997ed502df4931da7c6e203
Nurbergen Khinatolla <nhinatolla@gmail.com>
2021-09-02 14:07:27 +0600

    removed csrf

--- csv_example/urls.py
+++ csv_example/urls.py
@@ -21,4 +21,4 @@ urlpatterns += [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
