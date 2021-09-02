041d1b9bd46472d7ccbbd7fdfcead8f13c2c612f
Nurbergen Khinatolla <nhinatolla@gmail.com>
2021-09-02 12:30:00 +0600

    import os

--- csv_example/settings.py
+++ csv_example/settings.py
@@ -24,7 +24,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
