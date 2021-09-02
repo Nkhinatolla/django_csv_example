b7de582c60338bafb015216b5bb2e95d17f07fbf
Nurbergen Khinatolla <nhinatolla@gmail.com>
2021-09-02 16:04:40 +0600

    static configured

--- csv_example/urls.py
+++ csv_example/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.contrib.auth import views
 from rest_framework.authtoken.views import obtain_auth_token
@@ -18,5 +20,7 @@ urlpatterns = [
 urlpatterns += [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
