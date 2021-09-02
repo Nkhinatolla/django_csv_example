792caee4b6d53808b705d0aa08535c3e92216f8a
Nurbergen Khinatolla <nhinatolla@gmail.com>
2021-09-02 13:21:33 +0600

    static

--- csv_example/urls.py
+++ csv_example/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.contrib.auth import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +21,4 @@ urlpatterns += [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
