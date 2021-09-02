99478406dd3b5076e2b138ae9489b3a7aaa0bdf9
Nurbergen Khinatolla <nhinatolla@gmail.com>
2021-09-02 12:28:45 +0600

    static file

--- csv_example/settings.py
+++ csv_example/settings.py
@@ -80,7 +80,6 @@ REST_FRAMEWORK = {
 }
 
 ROOT_URLCONF = 'csv_example.urls'
-STATIC_ROOT = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -108,15 +107,19 @@ WSGI_APPLICATION = 'csv_example.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# postgres://btgbmwgpnovxqm:e82860d0e067eeb0e79eda96562f162e16578f33edc3f7d1ac9a93603d8b282b@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/ddq2vsstcsvd8g
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddq2vsstcsvd8g',
+        'HOST': 'ec2-54-74-77-126.eu-west-1.compute.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'btgbmwgpnovxqm',
+        'PASSWORD': 'e82860d0e067eeb0e79eda96562f162e16578f33edc3f7d1ac9a93603d8b282b'
+    },
 }
-
-# Password validation
+    # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -153,6 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
